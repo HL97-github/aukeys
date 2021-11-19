78b36944094b3d7b12915b4e2119b2bad43ca6be
18373704595@163.com <qq.123456>
2021-11-19 18:35:36 +0800

    新建项目

---
+++ spring-boot-parent/Demo/src/main/java/com/aukeys/it/SpringbootApplication.java
@@ -0,0 +1,11 @@
+package com.aukeys.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class,args);
+    }
+}
